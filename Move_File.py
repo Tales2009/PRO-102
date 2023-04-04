eff8a0adc68cbe93f920674fd32c7120036a1f82
Tales2009 <99425243+Tales2009@users.noreply.github.com>
2023-04-04 17:10:44 -0300

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+
+from_dir = "C:/Users/tartu/Dowloads"
+to_dir = "C:Users/tartu/Desktop/Arquivos_documentos"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
+    print(name)
+    print(extension)
+    if extension == "":
+        continue
+    if extension in ['.txt', 'doc', 'docx', 'pdf']:
+        path1 = from_dir + '/' + file_name
+        path2 = to_dir + '/' +"Arquivos_documentos"
+        path3 = to_dir + '/' + "Arquivos_documentos"+ file_name
+        print("Path1", path1)
+        print("path2", path2)
+        print("path3", path3)
+
+        if os.path.exists(path2):
+            print("Movendo"+ file_name + ".....")
+            shutil.move(path1, path3)
+        else:
+            os.makedirs(path2)
+            print("Movendo"+ file_name + "......")
+            shutil.move(path1, path3)
